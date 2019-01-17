378a3bde85b4891069f4d0a8e33a86971626c26a
vivek081166 <vivek081166@gmail.com>
2019-01-17 17:24:59 +0900

    something is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 
-import * as tf from '@tensorflow/tfjs';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,36 +7,7 @@ import * as tf from '@tensorflow/tfjs';
 })
 export class AppComponent implements OnInit {
 
-  model: tf.Model;
-  predictions: any;
-
   ngOnInit() {
-    this.loadModel();
-  }
-
-  // Load pretrained KERAS model
-  async loadModel() {
-    this.model = await tf.loadModel('./assets/model.json');
-  }
-
-  // Do predictions
-  async predict(imageData: ImageData) {
-
-    const pred = await tf.tidy(() => {
-
-      // Convert the canvas pixels to 
-      let img = tf.fromPixels(imageData, 1);
-      // @ts-ignore
-      img = img.reshape([1, 28, 28, 1]);
-      img = tf.cast(img, 'float32');
-
-      // Make and format the predications
-      const output = this.model.predict(img) as any;
-
-      // Save predictions on the component
-      this.predictions = Array.from(output.dataSync());
-    });
-
   }
 
 }
